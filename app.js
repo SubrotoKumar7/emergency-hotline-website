7719efb1d9e4d7f56d8aabecebf2333b96cf92b4
Subroto Kumar <subrotokumar021@gmail.com>
2025-08-27 23:23:30 +0600

    added call feature for phone icon

--- app.js
+++ app.js
@@ -20,10 +20,44 @@ cardContainer.addEventListener('click', function(event){
         heart.innerHTML = totalHeart;
     }
 
+    // for call button
     if(event.target.className.includes('call')){
         let title = event.target.parentNode.parentNode.childNodes[1].innerHTML;
         let subtitle = event.target.parentNode.parentNode.childNodes[3].innerHTML;
         let number = event.target.parentNode.parentNode.childNodes[5].childNodes[1].innerHTML;
+
+        let date = new Date().toLocaleTimeString();
+        let coinNum = Number(coin.innerHTML);
+
+
+        if(coinNum < 20){
+            alert("❌ You don't have enough coins. A minimum of 20 coins is required to make a call.");
+            return;
+        }
+        let totalCoin = coinNum - 20;
+        coin.innerHTML = totalCoin;
+
+        let div = document.createElement('div');
+        div.innerHTML = `
+                    <div class="flex items-center justify-between bg-gray-100 rounded shadow p-3 my-4">
+                        <div>
+                            <h1 class="font-semibold pt-2">${title}</h1>
+                            <p class="text-gray-500 my-2 text-lg">${number}</p>
+                        </div>
+                        <p class="text-lg">${date}</p>
+                    </div>
+        `;
+        historyContainer.appendChild(div);
+
+        let alertMessage = `📞 Calling ${subtitle} ${number}`;
+        alert(alertMessage);
+    }
+
+    // for call icon 
+    if(event.target.className.includes('phone-icon')){
+        let title = event.target.parentNode.parentNode.parentNode.childNodes[1].innerHTML;
+        let subtitle = event.target.parentNode.parentNode.parentNode.childNodes[3].innerHTML;
+        let number = event.target.parentNode.parentNode.parentNode.childNodes[5].childNodes[1].innerHTML;
 
         let date = new Date().toLocaleTimeString();
         let coinNum = Number(coin.innerHTML);
